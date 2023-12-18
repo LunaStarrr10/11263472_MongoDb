fc15ceafc90814ba2ebba8db9cc110fde0e9763f
Melissa Otoo <otoomelissa@gmail.com>
2023-12-18 09:34:39 +0000

    push later

--- db.js
+++ db.js
@@ -1,4 +1,18 @@
 import moongoose from "mongoose";
 
+const mongoose = require('mongoose');
+const mongoURL= "mongodb+srv://DaMuse:kobiborga@melissaapi.xr35jyw.mongodb.net/Wacko-Node?retryWrites=true&w=majority"
 
-
+function connect () {
+  mongoose.connect(mongoURL, {
+  useNewUrlParser:true,useUnifiedTopology:true
+  })
+  .then(() => {
+console.log("connected to MongoDb");
+  }
+  )
+  .catch(error) => {
+    console.error("Error connecting to Mongo:", error);
+  }
+}
+module.exports = {connect}
